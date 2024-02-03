38eb4665eb5444e2b6369ca096410dcc83b14067
Rabby <rabby.raziul@gmail.com>
2024-02-03 22:40:11 +0600

    Quiz App Flutter

---
+++ lib/start_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            //tranparency image
+            color: const Color.fromARGB(138, 255, 255, 255),
+          ),
+
+          // Opacity(
+          //   opacity: 0.5,
+          //   child: Image.asset(
+          //     'assets/images/quiz-logo.png',
+          //     width: 300,
+          //   ),
+          // ),
+          const SizedBox(
+            height: 70,
+          ),
+          Text(
+            '🫵 Learn Flutter the fun way! ✌️',
+            style: GoogleFonts.silkscreen(
+              color: const Color.fromARGB(255, 255, 255, 255),
+              fontSize: 18,
+            ),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton.icon(
+            onPressed: startQuiz,
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz'),
+          ),
+        ],
+      ),
+    );
+  }
+}
